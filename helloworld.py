11118960cf064298e4bc63bbbb8c9287dabb3b0c
Karun Subramanian <kas7176@g.harvard.edu>
2024-09-13 12:23:48 -0400

    Added helloworld optional challenge

--- helloworld.py
+++ helloworld.py
@@ -7,3 +7,5 @@ import sys
 name = sys.argv[1]
 
 print(f"Hello, {name}!")
+
+
---
+++ helloworld_challenge.py
@@ -0,0 +1,11 @@
+# helloworld program using pycharm
+# Optional challenge implementation
+
+import sys
+
+# Greet individually using the arguments passed
+
+for name in sys.argv[1:]:
+    print(f"Hello, {name}!")
+
+
